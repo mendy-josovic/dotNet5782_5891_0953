45a6bce283f14f7f328efe48c45178cdbca9cffa
mendy-josovic <mjosovic@j.jct.ac.il>
2021-10-04 14:59:59 +0300

    intial code+ 2 welcome func

---
+++ Targil0/Program5891.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Targil0
+{
+  partial  class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5891();
+            Welcome0953();
+            Console.ReadKey();
+
+
+        }
+        static partial void Welcome0953();
+        private static void Welcome5891()
+        {
+            Console.WriteLine("Enter your name");
+            string mystr = Console.ReadLine();
+            Console.WriteLine("{0}, Welcome to my first console application", mystr);
+        }
+    }
+}
