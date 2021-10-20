d60d828b669bfc21807a822261183620cf01deba
Daniel Bodenstein <danielboden111@gmail.com>
2021-10-20 11:54:44 +0300

    pull

---
+++ DAL/DalObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalObject
+{
+    public class DalObject
+    {
+        public DalObject() 
+        {
+            DataSource.Initialize();
+        }
+
+        public void AddStation(IDAL.DO.Station sta)
+        {
+            DataSource.Stations[DataSource.Config.StationsIndex] = sta;
+        }
+       
+        public void AddDrone(IDAL.DO.Drone dro)
+        {
+            DataSource.Drones[DataSource.Config.DronesIndex] = dro;
+        }
+
+        public void AddCustomer(IDAL.DO.Customer cst)
+        {
+            DataSource.Customers[DataSource.Config.CustomersIndex] = cst;
+        }
+
+        public void AddSParcel(IDAL.DO.Parcel prs)
+        {
+            DataSource.Parcels[DataSource.Config.ParcelsIndex] = prs;
+        }
+
+        public void UpdateStation(IDAL.DO.Station sta)
+        {
+            public int i = 0;
+            while()
+                i++;
+        }
+    }
+}
--- DAL/Station.cs
+++ DAL/Station.cs
@@ -10,11 +10,12 @@ namespace IDAL
     {
         public struct Station
         {
-            public int Id { get; set; }
+            public int Id { get; set; };
             public string Name { get; set; }
             public double Longitude { get; set; }
             public double Lattitude { get; set; }
             public int ChargeSlots { get; set; }
+            public Station(int id = 0) => this.Id = id;
         }
     }
 }
