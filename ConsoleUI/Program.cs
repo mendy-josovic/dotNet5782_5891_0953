3d27a857a80b5e6e0a67e8228dde7f6b6f029761
Daniel Bodenstein <danielboden111@gmail.com>
2021-11-07 12:30:55 +0200

    lists

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -154,7 +154,6 @@ namespace ConsoleUI
                                         {                                           
                                             Console.WriteLine("Enter drone ID");                                           
                                             int.TryParse(Console.ReadLine(), out int dtoneId);
-                                            dalObject1.DroneStatusMaintenanse(dtoneId);
                                             Console.WriteLine("Enter ID of a station from the list:");
                                             dalObject1.PrintAvailableChargingStations();
                                             int.TryParse(Console.ReadLine(), out int stationId);
@@ -166,9 +165,7 @@ namespace ConsoleUI
                                         {
                                             Console.WriteLine("Enter drone ID");
                                             int.TryParse(Console.ReadLine(), out int droneId);                        
-                                            dalObject1.DroneStatusAvailable(droneId);
                                             dalObject1.ClearDroneCharge(droneId);
-                                            dalObject1.UpdateDroneChargesIndex(droneId);
                                             break;
                                         }
                                     case 'f':
@@ -176,7 +173,6 @@ namespace ConsoleUI
                                             Console.WriteLine("Enter parcel ID");
                                             int.TryParse(Console.ReadLine(), out int parcelId);
                                             dalObject1.UpdateTimeOfSupplied(parcelId);
-                                            dalObject1.DroneStatusAvailable1(parcelId);
                                             break;
                                         }
                                 }
