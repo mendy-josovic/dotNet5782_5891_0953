e5f8711215cc7ac0dd0d5d3ca76bccecc617741c
Daniel Bodenstein <danielboden111@gmail.com>
2021-11-21 00:03:48 +0200

    consumption

--- BL/DisplyBl.cs
+++ BL/DisplyBl.cs
@@ -9,22 +9,16 @@ namespace BL
 {
     public partial class BL : IBL
     {
+        /// <summary>
+        /// the func gets the status of the parcel and acourdingly returns the consumption
+        /// </summary>
+        /// <param name="a">location a</param>
+        /// <param name="b">location b</param>
+        /// <param name="mode">mode of the drone in moving: without a parcel, or with a parcel, and which mode of parcel</param>
+        /// <returns>how much battery this moving need</returns>
         public double Consumption(Location a, Location b, MODE_OF_DRONE_IN_MOVING mode)
         {
             return GetDistance(a, b) * batteryConfig[(int)mode];
-
-        }
-        /// <summary>
-        /// the func gets the statis of the parcel and acourdingly 
-        /// returns the consumption
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <param name="mode"></param>
-        /// <returns></returns>
-        public int Consumption(Location a, Location b, MODE_OF_DRONE_IN_MOVING mode)
-        {
-            return (int)(GetDistance(a, b) * batteryConfig[(int)mode]);
         }
     }
 }
