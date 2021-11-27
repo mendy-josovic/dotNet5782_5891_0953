1be8720a71ef2515dd7ffbc0f38809cb7eb171ed
Daniel Bodenstein <danielboden111@gmail.com>
2021-11-27 22:02:49 +0200

    PL

--- DAL/Parcel.cs
+++ DAL/Parcel.cs
@@ -16,10 +16,10 @@ namespace IDAL
             public WEIGHT Weigh { set; get; }
             public PRIORITY Priority { set; get; }
             public int DroneId { get; set; }
-            public DateTime Requested { set; get; }
-            public DateTime Scheduled { set; get; }
-            public DateTime PickedUp { set; get; }
-            public DateTime Delivered { set; get; }
+            public DateTime? Requested { set; get; }
+            public DateTime? Scheduled { set; get; }
+            public DateTime? PickedUp { set; get; }
+            public DateTime? Delivered { set; get; }
             public override string ToString()//override the to-string to print it nice
             {
                 return "ID: " + Id + "\nSender ID: " + SenderId + " \nTarget ID: " + TargetId + "\nWeigh: " + Weigh + "\nPriority: " + Priority + "\nDrone ID: " + DroneId + "\nTime of requested: " + Requested + "\nTime of scheduled: " + Scheduled + "\nTime of picked up: " + PickedUp + "\nTime of delivered: " + Delivered + "\n";
