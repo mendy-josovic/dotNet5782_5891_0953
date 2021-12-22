276084460d1df1cd481c181880423e7977d15c1a
mendy-josovic <mjosovic@j.jct.ac.il>
2021-12-22 21:31:21 +0200

    hi

---
+++ PL/CustomerPl/CustomerListWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BlApi;
+using BO;
+namespace PL
+{
+    /// <summary>   
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public class CustomerListWindow : Window
+    {
+        IBl blobject;
+        public CustomerListWindow(IBl blobject)
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.blobject = blobject;
+
+            
+        }
+
+        private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void AddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- PL/ParclePl/ParcelListWindow.xaml.cs
+++ PL/ParclePl/ParcelListWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for ParcelListWindow.xaml
     /// </summary>
-    public partial class Page1 : Page
+    public partial class ParcelListWindow : Window
     {
-        public Page1()
+        public ParcelListWindow()
         {
             InitializeComponent();
         }
